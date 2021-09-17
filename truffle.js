3d2a2a03aafac06282d18319d8e63acdfa92bb20
FitFingers <hooper.j.1991@gmail.com>
2021-09-17 16:17:43 +0200

    feat: add truffle, adjust gitignore

---
+++ truffle.js
@@ -0,0 +1,50 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const MNEMONIC = process.env.MNEMONIC;
+const NODE_API_KEY = process.env.ALCHEMY_KEY;
+
+const needsNodeAPI =
+  process.env.npm_config_argv &&
+  (process.env.npm_config_argv.includes("rinkeby") ||
+    process.env.npm_config_argv.includes("live"));
+
+if ((!MNEMONIC || !NODE_API_KEY) && needsNodeAPI) {
+  console.error("Please set a mnemonic and ALCHEMY_KEY");
+  process.exit(0);
+}
+
+const rinkebyNodeUrl = `https://eth-rinkeby.alchemyapi.io/v2/${NODE_API_KEY}`;
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(MNEMONIC, rinkebyNodeUrl);
+      },
+      gas: 5000000,
+      network_id: 4,
+    },
+  },
+  mocha: {
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      gasPrice: 2,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 20, // Optimize for how many times you intend to run the code
+        },
+      },
+    },
+  },
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: "ETHERSCAN_API_KEY_FOR_VERIFICATION",
+  },
+};
