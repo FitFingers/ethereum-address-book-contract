e5c159578734c0009609194fe417435aa1bb8a84
FitFingers <hooper.j.1991@gmail.com>
2021-10-08 20:39:05 +0200

    feat: reduce 6% - rm modifiers, array

--- contracts/AddressBookFactory.sol
+++ contracts/AddressBookFactory.sol
@@ -13,21 +13,12 @@ contract AddressBookFactory {
     uint256 public accountOpenCost;
     uint256 public txCost;
 
-    // TODO: is this bloat? Can't I just use a mapping? If so, how do I handle verification such as existing account?
-    // Array of existing AddressBooks
-    AddressBook[] private addressBooks; // onlyOwner?
-
     // Take user address, return index of AddressBook => used to fetch data on F/E
-    mapping(address => uint256) private addressToIndex;
+    mapping(address => AddressBook) private addressBooks;
 
-    // Get this user's Address Book and return it
-    function fetchAddressBook()
-        public
-        view
-        customerOrOwner
-        returns (AddressBook userData)
-    {
-        userData = addressBooks[addressToIndex[msg.sender]];
+    // Return this user's Address Book contract address. Default is 0x0000000000000000000000000000000000000000
+    function fetchAddressBook() public view returns (AddressBook userData) {
+        userData = addressBooks[msg.sender];
         return userData;
     }
 
@@ -38,10 +29,10 @@ contract AddressBookFactory {
         owner = msg.sender;
         totalAddressBooks = 0;
         accountOpenCost = 0.00025 * 10**9 * 10**9; // in ETH
-        txCost = 0.0001 * 10**9 * 10 * 9; // in ETH
+        txCost = 0.0001 * 10**9 * 10**9; // in ETH
 
         // first address book belongs to owner;
-        addressBooks.push(new AddressBook(msg.sender));
+        addressBooks[msg.sender] = new AddressBook(msg.sender);
         totalAddressBooks++;
     }
 
@@ -56,58 +47,20 @@ contract AddressBookFactory {
         _;
     }
 
-    // Only permitted for customers or the owner
-    modifier customerOrOwner() {
-        require(
-            addressToIndex[msg.sender] > 0 || msg.sender == owner,
-            "You must have an account or be the owner to use this"
-        );
-        _;
-    }
-
-    // Only permitted for new/non-customers
-    modifier noncustomersOnly() {
-        require(
-            addressToIndex[msg.sender] == 0 && msg.sender != owner,
-            "You already have an account"
-        );
-        _;
-    }
-
-    // Only permitted for customers
-    modifier customerOnly() {
-        require(
-            addressToIndex[msg.sender] > 0,
-            "You must have an account in order to call this function"
-        );
-        _;
-    }
-
     // ADDRESS BOOK MANAGEMENT
 
     // Create a new AddressBook struct for this user
     function createAddressBook()
         public
         payable
-        noncustomersOnly
         returns (address contractAddress)
     {
-        require(msg.value >= accountOpenCost);
+        require(msg.value >= accountOpenCost, "Not enough ETH");
         AddressBook newBook = new AddressBook(msg.sender);
-        addressBooks.push(newBook);
-        addressToIndex[msg.sender] = totalAddressBooks;
+        addressBooks[msg.sender] = newBook;
         totalAddressBooks++;
         contractAddress = address(newBook);
         return contractAddress;
-    }
-
-    // Remove this user's AddressBook
-    function removeAddressBook(address _address) public customerOnly {
-        AddressBook latestNewUser = addressBooks[totalAddressBooks];
-        addressBooks[addressToIndex[_address]] = latestNewUser;
-        addressToIndex[_address] = 0;
-        addressBooks.pop();
-        totalAddressBooks--;
     }
 
     // UPDATE VARIABLE FUNCTIONS
